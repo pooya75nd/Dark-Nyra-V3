59892f2cff56b98b1b05c9ae6ac1ed3a82e3b6f9
pooya75nd <jules.courtot@gmail.com>
2025-09-29 22:28:52 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Orbitron', 'ui-sans-serif', 'system-ui']
+      },
+      colors: {
+        neon: '#39ff14',
+        purpledark: '#2a003f'
+      }
+    },
+  },
+  plugins: [],
+}
